35e9179d3a94453f631e961a9c02d36b9490cd08
Kailoke <f476947408@163.com>
2026-08-23 09:56:29 +0800

    license: add Kailoke copyright alongside upstream, add LICENSE file
    MIT requires the original copyright (skanmera) to be kept; add Copyright (c)2026 Kailoke as the current maintainer. Added root LICENSE file and updated AssemblyCopyright metadata in all three projects.

--- ExcelDiff/Properties/AssemblyInfo.cs
+++ ExcelDiff/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExcelDiff")]
-[assembly: AssemblyCopyright("Copyright ©skanmera  2017")]
+[assembly: AssemblyCopyright("Copyright ©skanmera  2017; Kailoke 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
